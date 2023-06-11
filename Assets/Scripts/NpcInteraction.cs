d7b25fa3272353b14712711f3152724bd02bcede
ismv <igor.shmoilov@binary-studio.com>
2023-06-11 13:06:47 +0300

    Scripts for QuestSystem and QuestItems added. NpcInteraction script updated

--- Assets/Scripts/NpcInteraction.cs
+++ Assets/Scripts/NpcInteraction.cs
@@ -1,19 +1,21 @@
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NpcInteraction : MonoBehaviour
 {
     public GameObject player;
-    public TMP_Text interactionText;
+    //public Text interactionText;
+    public GameObject questItemPrefab; // Prefab for the quest item
 
     private bool interactionEnabled = false;
+    private bool questGiven = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
         {
             interactionEnabled = true;
-            interactionText.gameObject.SetActive(true);
+            //interactionText.gameObject.SetActive(true);
         }
     }
 
@@ -22,7 +24,7 @@ public class NpcInteraction : MonoBehaviour
         if (other.gameObject == player)
         {
             interactionEnabled = false;
-            interactionText.gameObject.SetActive(false);
+            //interactionText.gameObject.SetActive(false);
         }
     }
 
@@ -30,13 +32,40 @@ public class NpcInteraction : MonoBehaviour
     {
         if (interactionEnabled && Input.GetKeyDown(KeyCode.E))
         {
-            InteractWithPlayer();
+            if (!questGiven)
+            {
+                GiveQuest();
+            }
+            else
+            {
+                InteractWithPlayer();
+            }
         }
+    }
+
+    private void GiveQuest()
+    {
+        // Instantiate the quest item in the scene
+        Instantiate(questItemPrefab, transform.position, Quaternion.identity);
+
+        // Display a message or UI feedback indicating the player received a quest
+        Debug.Log("Quest received!");
+
+        // Set questGiven flag to true
+        questGiven = true;
     }
 
     private void InteractWithPlayer()
     {
-        // TODO: Implement your interaction logic here
-        Debug.Log("Interaction with player activated!");
+        // Check if the quest is complete
+        QuestSystem questSystem = FindObjectOfType<QuestSystem>();
+        if (questSystem != null && questSystem.requiredItemCount > 0)
+        {
+            Debug.Log("You need to collect more items to complete the quest.");
+        }
+        else
+        {
+            Debug.Log("Quest completed! Interact with the capsule again to finish the quest.");
+        }
     }
 }
